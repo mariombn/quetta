82cb1710104465ef640c210f38a1f1265573792a
Mario de Moraes <mariombn@gmail.com>
2026-09-03 07:29:14 -0300

    Initial Commit

---
+++ quetta/quettaApp.swift
@@ -0,0 +1,17 @@
+//
+//  quettaApp.swift
+//  quetta
+//
+//  Created by Mario de Moraes Barros Neto on 03/09/26.
+//
+
+import SwiftUI
+
+@main
+struct quettaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
